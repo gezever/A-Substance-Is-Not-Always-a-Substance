897ece5959dc1df1543e0be60b56cbc437ff8214
gezever <gezever@skynet.be>
2026-04-02 08:10:48 +0200

    clean jsonld

--- bash/jsonld-to-ttl.bash
+++ bash/jsonld-to-ttl.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd data/processed/rdf
-/opt/apache-jena-5.6.0/bin/riot --formatted=TURTLE substances.jsonld > substances.ttl
-/opt/apache-jena-5.6.0/bin/riot --formatted=TURTLE classyfire.jsonld > classyfire.ttl
 
+/opt/apache-jena-5.6.0/bin/riot --formatted=TURTLE data/processed/rdf/classyfire.jsonld > data/processed/rdf/classyfire.ttl
+/opt/apache-jena-5.6.0/bin/riot --formatted=JSONLD data/processed/rdf/classyfire.jsonld > /tmp/classyfire.jsonld
+mv /tmp/classyfire.jsonld data/processed/rdf/classyfire.jsonld
+
+
+/opt/apache-jena-5.6.0/bin/riot --formatted=TURTLE data/processed/rdf/substances.jsonld > data/processed/rdf/substances.ttl
+/opt/apache-jena-5.6.0/bin/riot --formatted=JSONLD data/processed/rdf/substances.jsonld > /tmp/substances.jsonld
+mv /tmp/substances.jsonld data/processed/rdf/substances.jsonld
